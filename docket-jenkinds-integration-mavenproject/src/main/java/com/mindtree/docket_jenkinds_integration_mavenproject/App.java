1f245f163cef4ac625dbd3ab8341e0064b21326f
Ubuntu <sathishkumar6mi@sathishkumar6mi.ekj5e0gvyfvu1kn0dttecth3bh.gx.internal.cloudapp.net>
2021-05-30 14:10:59 +0000

    pom file change

---
+++ docket-jenkinds-integration-mavenproject/src/main/java/com/mindtree/docket_jenkinds_integration_mavenproject/App.java
@@ -0,0 +1,13 @@
+package com.mindtree.docket_jenkinds_integration_mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
